17336298c2c3bd77b79f24a08982746a1aa1775a
gukhman <gukhman@gmail.com>
2024-10-04 17:00:59 +0200

    Сделал 3 кнопки для 3 заданий

--- app/src/main/java/com/example/filework/MainActivity.kt
+++ app/src/main/java/com/example/filework/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.example.filework
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
 import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
-import java.io.File
-import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonSave: Button
-    private lateinit var buttonRead: Button
-    private lateinit var textToSave: EditText
-    private lateinit var textFromFile: EditText
+    private lateinit var task1: Button
+    private lateinit var task2: Button
+    private lateinit var task3: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,63 +25,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        buttonSave = findViewById(R.id.buttonSave)
-        buttonRead = findViewById(R.id.buttonRead)
-        textToSave = findViewById(R.id.textToSave)
-        textFromFile = findViewById(R.id.textFromFile)
+        task1 = findViewById(R.id.task1)
+        task2 = findViewById(R.id.task2)
+        task3 = findViewById(R.id.task3)
 
-        buttonSave.setOnClickListener {
-            writeToFileInDownload("example.txt", textToSave.text.toString())
-        }
-
-        buttonRead.setOnClickListener {
-            val text = readFromFile("example.txt")
-            textFromFile.setText(text)
+        task1.setOnClickListener {
+            val intent = Intent(this, Task1::class.java)
+            startActivity(intent)
         }
     }
-
-    private fun writeToFileInDownload(fileName: String, content: String) {
-        // Определяем каталог Downloads
-        val downloadsDirectory =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-
-        // Создаем файл
-        val file = File(downloadsDirectory, fileName)
-
-        try {
-            // Записываем текст в файл
-            file.writeText(content)
-
-            // Уведомляем пользователя об успешной записи
-            showSnackbar("Файл записан: ${file.absolutePath}")
-        } catch (e: IOException) {
-            e.printStackTrace()
-            showSnackbar("Ошибка записи файла: ${e.message}")
-        }
-    }
-
-    private fun showSnackbar(message: String) {
-        val rootView = findViewById<android.view.View>(android.R.id.content)
-        Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show()
-    }
-
-    private fun readFromFile(fileName: String): String {
-        // Определяем каталог Downloads
-        val downloadsDirectory =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-
-        // Создаем объект File для указанного файла
-        var text = ""
-        val file = File(downloadsDirectory, fileName)
-        if (file.exists()) {
-            try {
-                text = file.readText()
-                showSnackbar("Файл прочитан")
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-        return text
-    }
-
 }
